fb386bcea0e392119a8a8bf4e26075e70c551276
Subhransu Sekhar Rath <ssrath38@gmail.com>
2024-09-17 22:35:41 +0530

    Import syntax issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Header from "./Components/Header/Header";
-import Nav from "./Components/Nav/Nav";
-import About from "./Components/About/About";
-import Skill from "./Components/Skill/Skill";
-import Project from "./Components/Project/Project";
-import Contact from "./Components/Contact/Contact";
-import Footer from "./Components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
+import About from "./components/About/About";
+import Skill from "./components/Skill/Skill";
+import Project from "./components/Project/Project";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
